ddd30568de5124012b30997a57b51fef9201e8ea
Will Brown <wtbrown10@gmail.com>
2020-04-17 20:16:10 -0400

    first commit

---
+++ domweb.js
@@ -0,0 +1,3 @@
+document.getElementById("food").innerText = "Seafood";
+document.getElementById("activity").innerText = "Solving Algorithms";
+document.getElementById("Shows").innerText = "Youtube";
---
+++ jshotel.js
@@ -0,0 +1,4 @@
+document.getElementById("slogan").innerText = "Where Things Go Bump In The Night!"
+document.getElementById("Rooms")[2].innerText = "Room 210"
+document.getElementById("Rooms")[3].innerText = "Room 211"
+document.getElementById("Rooms")[4].innerText = "Room 212"
